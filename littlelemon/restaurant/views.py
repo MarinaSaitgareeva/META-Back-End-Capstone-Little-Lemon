f586ca00aff32e4f24586f88ea9037431b0ecc7c
Marina Saitgareeva <4006761ms@gmail.com>
2023-01-28 23:34:24 -0500

    user, menu, booking modelviewset

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from rest_framework import views, viewsets, generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
