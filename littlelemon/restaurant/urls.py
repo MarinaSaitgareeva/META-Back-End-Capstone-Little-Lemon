9db6ceb6842a11d9d99542a21748448563df6d81
Marina Saitgareeva <4006761ms@gmail.com>
2023-01-28 23:33:25 -0500

    user, menu, booking urls

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 
+router = DefaultRouter(trailing_slash=False)
+router.register("user", views.UserViewSet, basename="user")
+router.register("menu", views.MenuViewSet, basename="menu")
+router.register("booking", views.BookingViewSet, basename="booking")
+
+
 urlpatterns = [
+    path("", include(router.urls)),
     path("", views.index, name="index"),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
