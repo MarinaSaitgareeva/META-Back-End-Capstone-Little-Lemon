cc29c484680ba762aa8dd2c8c8445038190e440f
Marina Saitgareeva <4006761ms@gmail.com>
2023-01-30 10:20:17 -0500

    add tests for booking

--- littlelemon/littlelemon/tests/test_view.py
+++ littlelemon/littlelemon/tests/test_view.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-from restaurant.models import Menu
-from restaurant.serializers import MenuSerializer
+from datetime import date
+
+from restaurant.models import Menu, Booking
+from restaurant.serializers import MenuSerializer, BookingSerializer
 
 
 # Create your tests here.
@@ -11,4 +13,14 @@ class MenuViewSetTest(TestCase):
     def test_get_all(self):
         items =  Menu.objects.all()
         serialized_items = MenuSerializer(items, many=True)
+        self.assertEqual(len(serialized_items.data), 1)
+
+
+class BookingViewSetTest(TestCase):
+    def setUp(self):
+        Booking.objects.create(name="TestBookingName", no_of_guests=2, booking_date=date.today())
+    
+    def test_get_all(self):
+        items =  Booking.objects.all()
+        serialized_items = BookingSerializer(items, many=True)
         self.assertEqual(len(serialized_items.data), 1)
